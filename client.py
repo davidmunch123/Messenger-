f58f1e19949a411268b4cffb3141c0bbcadb830c
davidmunch123 <114887818+davidmunch123@users.noreply.github.com>
2023-04-26 13:44:08 -0400

    Update client.py

--- client.py
+++ client.py
@@ -39,11 +39,7 @@ def listen_for_messages():
         print("\n" + message)
 
 # make a thread that listens for messages to this client & print them
-t = Thread(target=listen_for_messages)
-# make the thread daemon so it ends whenever the main thread ends
-t.daemon = True
-# start the thread
-t.start()
+t = Thread(target=listen_for_messages, daemon=True).start()
 
 while True:
     # input message we want to send to the server
