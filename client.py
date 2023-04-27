0d5a22f0f2388c5946fd9c489feaf99f3cd14fd9
davidmunch123 <114887818+davidmunch123@users.noreply.github.com>
2023-04-27 18:12:26 -0400

    Update client.py

--- client.py
+++ client.py
@@ -5,36 +5,30 @@ from datetime import datetime
 from colorama import Fore, init, Back 
 import rsa
 
-with open("./publickey.pem", rb) as filekey: 
-    publickey = rsa.PublicKey.load_pkcs1(filekey.read())
-with open("./privatekey.pem", rb) as filekey: 
-    privatekey = rsa.PrivateKey.load_pkcs1(filekey.read())
-    
 
-# init colors
+with open("/home/munch/Projects/David/Messnger/Messenger-/publickey.pem", 'rb') as filekey: 
+    publickey = rsa.PublicKey.load_pkcs1(filekey.read())
+with open("/home/munch/Projects/David/Messnger/Messenger-/privatekey.pem", 'rb') as filekey: 
+    privatekey = rsa.PrivateKey.load_pkcs1(filekey.read())
+
 init()
 
-# set the available colors
 colors = [Fore.BLUE, Fore.CYAN, Fore.GREEN, Fore.LIGHTBLACK_EX, 
     Fore.LIGHTBLUE_EX, Fore.LIGHTCYAN_EX, Fore.LIGHTGREEN_EX, 
     Fore.LIGHTMAGENTA_EX, Fore.LIGHTRED_EX, Fore.LIGHTWHITE_EX, 
     Fore.LIGHTYELLOW_EX, Fore.MAGENTA, Fore.RED, Fore.WHITE, Fore.YELLOW
 ]
 
-# choose a random color for the client
 client_color = random.choice(colors)
 
-# server's IP address
-# if the server is not on this machine, 
-# put the private (network) IP address (e.g 192.168.1.2)
 SERVER_HOST = "127.0.0.1"
-SERVER_PORT = 5002 # server's port
-separator_token = "<SEP>" # we will use this to separate the client name & message
+SERVER_PORT = 5002 
+separator_token = "<SEP>" 
 
-# initialize TCP socket
+
 s = socket.socket()
 print(f"[*] Connecting to {SERVER_HOST}:{SERVER_PORT}...")
-# connect to the server
+
 s.connect((SERVER_HOST, SERVER_PORT))
 print("[+] Connected.")
 
@@ -42,25 +36,21 @@ name = input('What is your name: ')
 
 def listen_for_messages():
     while True:
-        message = s.recv(1024).decode() 
-        message = rsa.decrypt(message, privatekey).decode('ascii') 
+        message = s.recv(1024)
+        message = rsa.decrypt(message, privatekey).decode('cp1252') 
+        message = message.replace(separator_token, ': ')
         print("\n" + message)
 
-# make a thread that listens for messages to this client & print them
 t = Thread(target=listen_for_messages, daemon=True).start()
 
 while True:
-    # input message we want to send to the server
     to_send =  input()
-    # a way to exit the program
     if to_send.lower() == 'q':
         break
-    # add the datetime, name & the color of the sender
+    
     date_now = datetime.now().strftime('%Y-%m-%d %H:%M:%S') 
     to_send = f"{client_color}[{date_now}] {name}{separator_token}{to_send}{Fore.RESET}"
-    # finally, send the message
-    to_send = rsa.encrypt(str(to_send).encode('ascii'), publickey)
-    s.send(to_send.encode())
+    to_send = rsa.encrypt(bytes(to_send, 'cp1252'), publickey)
+    s.send(to_send)
 
-# close the socket
 s.close()
